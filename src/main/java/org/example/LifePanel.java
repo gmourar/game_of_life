d20deaec7c0906fdf803ad4e8c0216b898e1e115
Gabriel Moura <gabriel.moura@sighra.com.br>
2024-07-26 10:15:02 -0300

    working

--- src/main/java/org/example/LifePanel.java
+++ src/main/java/org/example/LifePanel.java
@@ -9,18 +9,23 @@ public class LifePanel extends JPanel implements ActionListener {
     int xPanel;
     int yPanel;
     int size = 10;
-    int[][] life = new int [xPanel/size][yPanel/size];
-    int [][] beforeLife =  new int[xPanel/size][yPanel/size];
+    int xWidth;
+    int yHeight;
+    int[][] life;
+    int[][] beforeLife;
     boolean starts = true;
 
     public LifePanel(int width, int height) {
         xPanel = width;
         yPanel = height;
-        life = new int[xPanel / size][yPanel / size];
+        xWidth = xPanel / size;
+        yHeight = yPanel / size;
+        life = new int[xWidth][yHeight];
+        beforeLife = new int[xWidth][yHeight];
         setSize(xPanel, yPanel);
         setLayout(null);
 
-        new Timer (80 , this).start();
+        new Timer(80, this).start();
     }
 
     public void paintComponent(Graphics g) {
@@ -33,84 +38,78 @@ public class LifePanel extends JPanel implements ActionListener {
         display(g);
     }
 
-    private void grid (Graphics g){
-        for (int i = 0; i < life.length; i++) {
-            g.drawLine( 0, i * size, xPanel , i * size ); // linha
-            g.drawLine(i * size, 0 , i * size, yPanel); // coluna
+    private void grid(Graphics g) {
+        for (int i = 0; i <= xWidth; i++) {
+            g.drawLine(i * size, 0, i * size, yPanel); // coluna
+        }
+        for (int i = 0; i <= yHeight; i++) {
+            g.drawLine(0, i * size, xPanel, i * size); // linha
         }
     }
 
-    private void spawn(Graphics g){
-        if (starts){
-            for (int x = 0; x < life.length; x++){
-                for (int y = 0; y < (yPanel / size ); y++){
-                    if ((int)(Math.random() * 10) == 0){
+    private void spawn(Graphics g) {
+        if (starts) {
+            for (int x = 0; x < xWidth; x++) {
+                for (int y = 0; y < yHeight; y++) {
+                    if ((int) (Math.random() * 10) == 0) {
                         beforeLife[x][y] = 1;
                     }
                 }
-
             }
             starts = false;
         }
     }
 
-    private void display (Graphics g){
+    private void display(Graphics g) {
         g.setColor(Color.GREEN);
         copyArray();
-        for (int x = 0; x < life.length; x++){
-            for (int y = 0; y < (yPanel / size ); y++){
-                if (life[x][y] == 1){
-                    g.fillRect(x * size , y * size , size, size);
+        for (int x = 0; x < xWidth; x++) {
+            for (int y = 0; y < yHeight; y++) {
+                if (life[x][y] == 1) {
+                    g.fillRect(x * size, y * size, size, size);
                 }
             }
-
         }
     }
 
-    private void copyArray(){
-        for (int x = 0; x < life.length; x++){
-            for (int y = 0; y < (yPanel / size ); y++){
-               life[x][y] = beforeLife[x][y];
+    private void copyArray() {
+        for (int x = 0; x < xWidth; x++) {
+            for (int y = 0; y < yHeight; y++) {
+                life[x][y] = beforeLife[x][y];
             }
-
         }
     }
 
-    private int check(int x , int y){
+    private int check(int x, int y) {
         int alive = 0;
-        alive += life[x - 1][y - 1];
-        alive += life[x][y - 1];
-        alive += life[x + 1][y - 1];
-        alive += life[x - 1][y];
-        alive += life[x + 1][y];
-        alive += life[x - 1][y + 1];
-        alive += life[x][y + 1];
-        alive += life[x + 1][y + 1];
+
+        alive += life[(x + xWidth - 1) % xWidth][(y + yHeight - 1) % yHeight];
+        alive += life[(x + xWidth) % xWidth][(y + yHeight - 1) % yHeight];
+        alive += life[(x + xWidth + 1) % xWidth][(y + yHeight - 1) % yHeight];
+        alive += life[(x + xWidth - 1) % xWidth][(y + yHeight) % yHeight];
+        alive += life[(x + xWidth + 1) % xWidth][(y + yHeight) % yHeight];
+        alive += life[(x + xWidth - 1) % xWidth][(y + yHeight + 1) % yHeight];
+        alive += life[(x + xWidth) % xWidth][(y + yHeight + 1) % yHeight];
+        alive += life[(x + xWidth + 1) % xWidth][(y + yHeight + 1) % yHeight];
 
         return alive;
     }
 
-    public void actionPerformed (ActionEvent e){
+    public void actionPerformed(ActionEvent e) {
         int alive;
-        for (int x = 0; x < life.length; x++){
-            for (int y = 0; y < (yPanel / size ); y++){
+        for (int x = 0; x < xWidth; x++) {
+            for (int y = 0; y < yHeight; y++) {
                 alive = check(x, y);
 
-            if (alive == 3){
-                beforeLife[x][y] = 1;
-            }
-            else if( alive == 2 && life[x][y] == 1) {
-                beforeLife[x][y] = 1;
-            }
-            else {
-                beforeLife[x][y] = 0;
-            }
-
-
+                if (alive == 3) {
+                    beforeLife[x][y] = 1;
+                } else if (alive == 2 && life[x][y] == 1) {
+                    beforeLife[x][y] = 1;
+                } else {
+                    beforeLife[x][y] = 0;
+                }
             }
         }
-
-
         repaint();
     }
 }
